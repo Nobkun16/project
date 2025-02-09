fdfb9e9f598ab486f7c43f6e205f04b5ad74aa8a
unknown <johnphilipbaylon17@gmail.com>
2025-02-09 16:31:57 +0800

    Your commit message

--- webbased/login/urls.py
+++ webbased/login/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+login_url_name="url"
+
 urlpatterns=[
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
